9a9363e78f422acadeb7321d509ff21b46f4fdec
“mustafadurmaz” <mdmz_001@hotmail.com>
2021-11-14 20:16:18 +0300

    Mongoose modülü kurularak mongodb bağlantısı yapıldı

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const ejs = require("ejs");
+const path = require('path');
+const mongoose=require('mongoose');
+const Post=require('./models/Post')
 
 const app = express();
+mongoose.connect('mongodb://localhost/cleanblog-test-db');
+
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', {
+    posts,
+  });
 });
 
 app.get("/about", (req, res) => {
@@ -20,6 +30,11 @@ app.get("/post", (req, res) => {
 
 app.get("/add_post", (req, res) => {
   res.render("add_post");
+});
+
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
